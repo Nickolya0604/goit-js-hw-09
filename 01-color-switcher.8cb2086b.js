75f785140b03ab2453dcb3aaa99e0942e4fcc9d0
Nickolya0604 <104914992+Nickolya0604@users.noreply.github.com>
2022-10-02 11:39:12 +0000

    Deploying to gh-pages from @ Nickolya0604/goit-js-hw-09@2105e407f1e9938dfb83334eb0c2c1a507916d30 🚀

---
+++ 01-color-switcher.8cb2086b.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("button[data-start]"),t=document.querySelector("button[data-stop]"),d=document.querySelector("body");t.disabled=!0,e.addEventListener("click",(()=>{timerId=setInterval((()=>{d.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(()=>{clearInterval(timerId),t.disabled=!0,e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.8cb2086b.js.map
